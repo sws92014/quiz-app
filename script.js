424e5339d297e5a9562202176a002cf887c7e939
Sarah Smith <sws92014@gmail.com>
2016-08-04 21:50:19 -0500

    cleaned up javascript functions and fixed stylesheet

--- script.js
+++ script.js
@@ -5,14 +5,40 @@ $(document).ready(function() {
     var currentQuestion = 0;
     var previousQuestion = currentQuestion - 1;
 
+
 //ENTER BUTTON EVENT//
     $("#introDiv").on("click", "#enter", function (event) {
-        $("#introDiv").fadeOut();
-        $('#questionDiv').fadeIn();
-        loadQuestion();
         event.preventDefault();
+        introFadesToQuestions();
+        loadQuestion();
         imgChange();
     });
+
+
+//SUBMIT BUTTON EVENT//
+    $("#questionDiv").on("click", "#submit", function (event) {
+        event.preventDefault();
+        answerCheck();
+    });
+
+
+//CONTINUE BUTTON EVENT//
+    $("#resultDiv").on("click", "#continue", function (event) {
+        event.preventDefault();
+        goToNextScreen();
+    });
+
+//RETRY BUTTON EVENT//
+    $("#endDiv").on("click", "#retry", function (event) {
+        event.preventDefault();
+        retry();
+    });
+
+//INTRO PAGE FADES AND QUESTION APPEARS FUNCTION//
+    function introFadesToQuestions() {
+        $("#introDiv").fadeOut();
+        $('#questionDiv').fadeIn();
+    }
 
 //IMAGE CHANGE FUNCTION//
     function imgChange() {
@@ -46,84 +72,84 @@ $(document).ready(function() {
 
 //QUESTIONS ARRAY//
        var questions = [{
-        question: "imgages/meme.png",
+        question: "meme",
         choices: ["may • may", "me • me", "me • may", "meem"],
         qNum : 1,
         correct : 3,
         fact: "It's 'Meem.' Nothing fancy. Just one little syllable."
         },
         {
-        question: "imgages/tumblr.png",
+        question: "tumblr",
         choices: ["tum • bler", "tum • bull • er", "tum • bull • ar", "tum • bler • peer • ee • od"],
         qNum : 2,
         correct : 0,
         fact: "It's 'tum • bler.' Nbdy hs tm fr vwls nymr."
         },
         {
-        question: "imgages/imgur.png",
+        question: "imgur",
         choices: ["image • er", "im • jur", "im • ger", "im • agg • ur"],
         qNum : 3,
         correct : 0,
-        fact: "It's 'image • er.' It’s pronounced like like the word “image” with an “-er” on the end."
+        fact: "It's 'image • er.' It’s pronounced like the word “image” with an “-er” on the end."
         },
         {
-        question: "imgages/data.png",
+        question: "data",
         choices: ["day • tay", "dah • tah", "day • tah", "dat • ah"],
         qNum : 4,
         correct : 2,
         fact: "It's 'day • tah.' Like the character from Star Trek: The Next Generation!"
         },
         {
-        question: "imgages/ascii.png",
+        question: "ascii",
         choices: ["ace • ee", "aye • es • see • eye • eye", "ass • key", "ass • see"],
         qNum : 5,
         correct : 2,
         fact: "It's 'ass • key.' This form of early internet text-only image creation may not be very popular these days, but that’s no excuse for pronouncing it incorrectly."
         },
         {
-        question: "imgages/qwerty.png",
+        question: "qwerty",
         choices: ["queue • dub • el • you • ee • arr • tee • why", "kware • tie", "kwer • dee", "kwer • tee"],
         qNum : 6,
         correct : 3,
         fact: "It's 'kwer • tee.' Say it just like it’s spelled right there on your keyboard."
         },
         {
-        question: "imgages/wifi.png",
+        question: "wifi",
         choices: ["wiff • fee", "why • fye", "dub • bull • you • eye • eff • eye", "why • fee"],
         qNum : 7,
         correct : 1,
-        fact: "It's 'why • fye.' Did you know that “wifi” isn’t actually short for anything? Most people think it’s short for 'wireless fidelity, but it’s not!"
+        fact: "It's 'why • fye.' Did you know that “wifi” isn’t actually short for anything? Most people think it’s short for 'wireless fidelity', but it’s not!"
         },
         {
-        question: "imgages/lol.png",
+        question: "lol",
         choices: ["lowel", "el • oh • el", "lahl", "loh • el"],
         qNum : 8,
         correct : 1,
         fact: "It's 'el • oh • el.' Just like you’re saying each letter you see."
         },
         {
-        question: "imgages/png.png",
-        choices: ["pnnj", "ping", "pee • en • jee", "pong"],
+        question: "png",
+        choices: ["pang", "ping", "pee • en • jee", "pong"],
         qNum : 9,
         correct : 1,
         fact: "It's 'ping.' Although, in a pinch, calling it a “pee • en • jee” works too."
         },
         {
-        question: "imgages/png.png",
+        question: "gif",
         choices: ["gif (like “gift”)", "jif (like “gin”)", "jee • eye • eff", "guh • if"],
         qNum : 10,
         correct : 0,
         fact: "It's 'gif (like “gift”).' A lot of people are going to try to convince you that it’s jif with a soft g. It’s not. Don’t listen to those people."
         },
         {
-        question: "imgages/exe.png",
+        question: "exe",
         choices: ["ex • ee", "eggs • ee", "ee • ex • ee", "ee • ex • eh"],
         qNum : 11,
         correct : 2,
         fact: "It's 'ee • ex • ee.' You say each individual letter."
         },
         {
-        question: "imgages/jpg.png",
+        question: "jpg",
         choices: ["jay • pedge", "je • pug", "jay • pee • ee • gee", "jay • peg"],
         qNum : 12,
         correct : 3,
@@ -133,15 +159,8 @@ $(document).ready(function() {
     }]
 
 
-//SUBMIT BUTTON EVENT//
-    $("#questionDiv").on("click", "#submit", function (event) {
-        answerCheck();
-        event.preventDefault();
-    });
-
-
-//CONTINUE BUTTON EVENT//
-    $("#resultDiv").on("click", "#continue", function (event) {
+//GO TO NEXT SCREEN/QUESTION FUNCTION//
+    function goToNextScreen() {
         if (currentQuestion < 11) {
             currentQuestion++;
             loadQuestion();
@@ -158,19 +177,20 @@ $(document).ready(function() {
             $('#endDiv').slideDown();
             $("#result").html("You correctly answered " +numberCorrect+ " out of 12");
         };
-        event.preventDefault();
-    });
-
-
-//RETRY BUTTON EVENT//
-    $("#endDiv").on("click", "#retry", function (event) {
-        retry();
-        event.preventDefault();
-    });
+    }
 
 //LOAD QUESTION FUNCTION//
     function loadQuestion() { 
-            var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span></br><div id="answerSelect"><input type="radio" name="option" class="option" value="0"><span class="answer">'+questions[currentQuestion].choices[0]+'</span><br><input type="radio" name="option" class="option" value="1"><span class="answer">'+questions[currentQuestion].choices[1]+'</span><br><input type="radio" name="option" class="option" value="2"><span class="answer">'+questions[currentQuestion].choices[2]+'</span><br><input type="radio" name="option" class="option" value="3"><span class="answer">'+questions[currentQuestion].choices[3]+'</span><br></div>';
+            var newQuestion = '<span class="question">'+
+            questions[currentQuestion].question+
+            '</span></br><div id="answerSelect"><input type="radio" name="option" class="option" value="0"><span class="answer">'+
+            questions[currentQuestion].choices[0]+
+            '</span><br><input type="radio" name="option" class="option" value="1"><span class="answer">'+
+            questions[currentQuestion].choices[1]+
+            '</span><br><input type="radio" name="option" class="option" value="2"><span class="answer">'+
+            questions[currentQuestion].choices[2]+
+            '</span><br><input type="radio" name="option" class="option" value="3"><span class="answer">'+
+            questions[currentQuestion].choices[3]+'</span><br></div>';
             $("#questionArea").html(newQuestion);   
     };
 
@@ -195,7 +215,6 @@ $(document).ready(function() {
         };
     }
     
-
 //RETRY FUNCTION//
     function retry() {
         numberCorrect = 0;
